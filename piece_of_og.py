6425e737e93de170f55f07383d83e7664902fb3d
diewland <diewland@gmail.com>
2025-04-28 16:24:48 +0700

    add piece_of_og script

--- piece_of_og.py
+++ piece_of_og.py
@@ -1,15 +1,14 @@
 import os, json, time
 import requests
 
-ALCHEMY_API = "https://soneium-mainnet.g.alchemy.com/nft/v2/{}/getOwnersForToken?contractAddress={}&tokenId={}&pageKey=1&pageSize={}"
+ALCHEMY_API = "https://opt-mainnet.g.alchemy.com/nft/v2/{}/getOwnersForToken?contractAddress={}&tokenId={}&pageKey=1&pageSize={}"
 ALCHEMY_KEY = os.getenv('KEY_ALCHEMY_POOPZTER')
-CONTRACT    = "0xA9213211Ac66aFB650C3b501bdD538013A35442f"
-FROM_ID     = 1
-TO_ID       = 3 # TODO
+CONTRACT    = "0x32F277cEce2Ba12029E07c6C1D1eBA63F1Bb518b"
+IDS         = [ 8, 9, 10 ] # TODO
 PAGE_SIZE   = 5_000
-OUT_PATH    = "./json/piece_on_soneium/{}.json"
+OUT_PATH    = "./json/piece_of_og/{}.json"
 
-for id in range(FROM_ID, TO_ID+1):
+for id in IDS:
     # craft url
     url = ALCHEMY_API.format(ALCHEMY_KEY, CONTRACT, id, PAGE_SIZE)
 
